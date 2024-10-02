e5a7434398c5bca057e0792c5c45f3dba728c8cc
Harsh M <harshmaroo123@gmail.com>
2024-10-02 13:36:21 +0530

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module pixcloud
+
+go 1.23.2
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1> Welcome to my new website </h1>")
+}
+
+func main() {
+	http.HandleFunc("/", handlerFunc)
+	fmt.Println("Starting server on 3000....")
+	http.ListenAndServe(":3000", nil)
+}

64cd3d7f35ff47b6b1664c80ec91e6795ca8bd19
Harsh M <harshmaroo123@gmail.com>
2024-11-03 14:33:28 +0530

    Installed chi for routing

--- main.go
+++ main.go
@@ -5,12 +5,33 @@ import (
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homePageHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1> Welcome to my new website </h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1> Contact page </h1>")
+}
+
+type Router struct{}
+
+func (_ Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homePageHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		http.NotFound(w, r)
+	}
+
+}
+
 func main() {
-	http.HandleFunc("/", handlerFunc)
+	// http.HandleFunc("/", pathhandler)
+	// http.HandleFunc("/", homePageHandler)
+	// http.HandleFunc("/contact", contactHandler)
+	var router Router
 	fmt.Println("Starting server on 3000....")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", router)
 }

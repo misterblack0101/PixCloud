e986c7c8076f83063ef1cdef5c44665488d9e278
Harsh M <harshmaroo123@gmail.com>
2024-11-16 12:07:05 +0530

    created a static handler as a controller

---
+++ controllers/static.go
@@ -0,0 +1,13 @@
+package controllers
+
+import (
+	"net/http"
+	"pixcloud/views"
+)
+
+func StaticHandler(tpl views.Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, nil)
+	}
+
+}
--- main.go
+++ main.go
@@ -2,41 +2,34 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"path/filepath"
+	"pixcloud/controllers"
 	"pixcloud/views"
 
 	"github.com/go-chi/chi/v5"
 )
 
-func executeTemplate(w http.ResponseWriter, filePath string) {
-	t, err := views.Parse(filePath)
-	if err != nil {
-		log.Print(err)
-		http.Error(w, "<h1> There was en error loading the site</h1>", http.StatusInternalServerError)
-		return
-	}
-	t.Execute(w, nil)
-}
-
-func homePageHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, filepath.Join("templates", "home.gohtml"))
-}
-
-func contactHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, filepath.Join("templates", "contact.gohtml"))
-}
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	executeTemplate(w, filepath.Join("templates", "faq.gohtml"))
-}
-
 func main() {
 	router := chi.NewRouter()
-	router.Get("/", homePageHandler)
-	router.Get("/contact", contactHandler)
-	router.Get("/faq", faqHandler)
+	t, err := views.Parse(filepath.Join("templates", "home.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	router.Get("/", controllers.StaticHandler(t))
+
+	t, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	router.Get("/contact", controllers.StaticHandler(t))
+
+	t, err = views.Parse(filepath.Join("templates", "faq.gohtml"))
+	if err != nil {
+		panic(err)
+	}
+	router.Get("/faq", controllers.StaticHandler(t))
+
 	router.NotFoundHandler()
 	fmt.Println("Starting server on 3000....")
 	http.ListenAndServe(":3000", router)

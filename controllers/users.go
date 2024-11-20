064965dfb4d6b87badcb137a4d7ac2b06fb914b0
Harsh M <harshmaroo123@gmail.com>
2024-11-20 10:08:18 +0530

    Adding email autofill via URL

--- controllers/users.go
+++ controllers/users.go
@@ -13,7 +13,11 @@ type Users struct {
 }
 
 func (user Users) New(w http.ResponseWriter, r *http.Request) {
-	user.Templates.New.Execute(w, nil)
+	var data struct {
+		Email string
+	}
+	data.Email = r.FormValue("email")
+	user.Templates.New.Execute(w, data)
 }
 
 func (user Users) Create(w http.ResponseWriter, r *http.Request) {
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module pixcloud
 
 go 1.23.2
 
-require github.com/go-chi/chi/v5 v5.1.0 // indirect
+require github.com/go-chi/chi/v5 v5.1.0
